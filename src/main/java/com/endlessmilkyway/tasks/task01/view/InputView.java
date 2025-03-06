913e4af22d57b5bdc3adb940597a36f143b3d059
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-03-06 17:00:53 +0900

    refactor: 입력 메시지 관련 코드 개선
    - 코드 내부에 존재하던 입력 관련 메시지들을 상수로 선언하여 코드 상단부에서 편리하게 관리할 수 있도록 하였다.

--- src/main/java/com/endlessmilkyway/tasks/task01/view/InputView.java
+++ src/main/java/com/endlessmilkyway/tasks/task01/view/InputView.java
@@ -1,10 +1,13 @@
-package com.endlessmilkyway.view;
+package com.endlessmilkyway.tasks.task01.view;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class InputView {
+
+    private static final String INPUT_ARRAY_LENGTH_MSG = "배열의 길이를 입력해주세요 : ";
+    private static final String INPUT_ARRAY_ELEMS_MSG = "배열의 항목들을 입력해주세요(띄어쓰기로 구분) : ";
 
     private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -23,7 +26,7 @@ public class InputView {
     public int getArrayLengthFromConsole() {
         int length = 0;
         try {
-            System.out.print("배열의 길이를 입력해주세요 : ");
+            System.out.print(INPUT_ARRAY_LENGTH_MSG);
             length = Integer.parseInt(br.readLine());
         } catch (IOException e) {
             System.out.println(e.getMessage());
@@ -35,7 +38,7 @@ public class InputView {
     public String getArrayElemsFromConsole() {
         String elems = "";
         try {
-            System.out.print("배열의 항목들을 입력해주세요(띄어쓰기로 구분) : ");
+            System.out.print(INPUT_ARRAY_ELEMS_MSG);
             elems = br.readLine();
         } catch (IOException e) {
             System.out.println(e.getMessage());
