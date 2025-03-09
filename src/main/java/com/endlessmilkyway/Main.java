6712a1e077961aecb96567084dd0bf1aff7a6aca
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-03-10 05:10:25 +0900

    feat: 태스크 2번 람다 표현식을 이용한 계산기 구현

--- src/main/java/com/endlessmilkyway/Main.java
+++ src/main/java/com/endlessmilkyway/Main.java
@@ -1,11 +1,14 @@
 package com.endlessmilkyway;
 
-import com.endlessmilkyway.tasks.task01.Task01;
+import com.endlessmilkyway.tasks.task02.Task02;
 
 public class Main {
 
     public static void main(String[] args) {
-        Task01 task01 = new Task01();
-        task01.run();
+//        Task01 task01 = new Task01();
+//        task01.run();
+
+        Task02 task02 = new Task02();
+        task02.run();
     }
 }
---
+++ src/main/java/com/endlessmilkyway/tasks/task02/Task02.java
@@ -0,0 +1,40 @@
+package com.endlessmilkyway.tasks.task02;
+
+import com.endlessmilkyway.tasks.task02.view.InputView;
+import java.util.List;
+import java.util.function.BiConsumer;
+
+public class Task02 {
+
+    private static final String HEADER = "연산 결과는 ";
+    private static final String FOOTER = " 입니다.";
+
+    public void run() {
+        InputView inputView = InputView.getInstance();
+        List<Long> numbers = inputView.getNumbersFromConsole();
+
+        String operator = inputView.getOperatorFromConsole();
+
+        switch (operator) {
+            case "+":
+                BiConsumer<Long, Long> sum = (x, y) -> System.out.println(HEADER + (x + y) + FOOTER);
+                sum.accept(numbers.get(0), numbers.get(1));
+                break;
+            case "-":
+                BiConsumer<Long, Long> subtract = (x, y) -> System.out.println(HEADER + (x - y) + FOOTER);
+                subtract.accept(numbers.get(0), numbers.get(1));
+                break;
+            case "*":
+                BiConsumer<Long, Long> multiple = (x, y) -> System.out.println(HEADER + (x * y) + FOOTER);
+                multiple.accept(numbers.get(0), numbers.get(1));
+                break;
+            case "/":
+                BiConsumer<Long, Long> divide = (x, y) -> System.out.println(HEADER + ((double) x / y) + FOOTER);
+                divide.accept(numbers.get(0), numbers.get(1));
+                break;
+            case "**":
+                BiConsumer<Long, Long> square = (x, y) -> System.out.println(HEADER + Math.pow(x, y) + FOOTER);
+                square.accept(numbers.get(0), numbers.get(1));
+        }
+    }
+}
